52b096f991bed1294c39043ebef9a04d278a1a4d
Umutcan Elci <umutcanelci.me@gmail.com>
2026-02-27 03:11:38 +0300

    even it is done there is a different problem here - the problem is about the .net itself will fix that first

--- SystemDefense/Program.cs
+++ SystemDefense/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using Tests;
+using Tools;
+
 Console.WriteLine("Hello, World!");
+
+var str = new StringArsenal();
+
+List<string> list = new List<string>();
+
+
+list.Add("file/new_file/a.txt");
+
+str.FindExtension(list);
 
--- SystemDefense/Tools/StringArsenal.cs
+++ SystemDefense/Tools/StringArsenal.cs
@@ -1,6 +1,19 @@
 namespace Tools;
 
-public class Substring
+public class StringArsenal
 {
+
+    // First find the lastvalue on the string array with lastIndexOf functions
+    // Then take the extension like .pdf or .cs or .png etc....
+
+    public void FindExtension(List<string> str)
+    {
+
+        string value = str[0];
+
+        int index = value.LastIndexOf(".");
+
+        Console.WriteLine(value.Substring(index));
+    }
 
 }
