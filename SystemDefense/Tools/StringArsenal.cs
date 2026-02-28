9ad9f5f5e620c6230bbf447ff069555431586998
Umutcan Elci <umutcanelci.me@gmail.com>
2026-02-28 17:30:25 +0300

    all done findExtension then I will cont.

--- SystemDefense/Tools/StringArsenal.cs
+++ SystemDefense/Tools/StringArsenal.cs
@@ -3,17 +3,33 @@ namespace Tools;
 public class StringArsenal
 {
 
-    // First find the lastvalue on the string array with lastIndexOf functions
-    // Then take the extension like .pdf or .cs or .png etc....
 
-    public void FindExtension(List<string> str)
+
+    public string FindExtension(string str)
     {
+        // First find the lastvalue on the string array with lastIndexOf functions
+        // Then take the extension like .pdf or .cs or .png etc....
 
-        string value = str[0];
+        if (string.IsNullOrEmpty(str))
+        {
+            return "";
+        }
 
-        int index = value.LastIndexOf(".");
+        int index = str.LastIndexOf(".");
 
-        Console.WriteLine(value.Substring(index));
+        if (index == -1)
+        {
+            return "";
+        }
+
+        int lastForwardSlash = str.LastIndexOf("/");
+
+        if (index < lastForwardSlash)
+        {
+            return "";
+        }
+        return str.Substring(index);
+
     }
 
 }
